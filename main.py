088a48ebff18e410d052bb141b4d92be8379d552
Parker Gornick <parkergornick@gmail.com>
2023-05-28 13:16:58 -0500

    Added def display_title

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# File: main.py
+# Description: Python grade assigment
+# Author: Parker Gornick
+# Date: 5/28/23
+
+
+def display_title():
+    print("Welcome to the Grade Calculator")
 
 
 
 
 
 if __name__ == "__main__":
+    display_title()
