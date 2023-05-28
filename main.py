8e32c69a4af12cacea0673a6d0e58072578bf981
Parker Gornick <parkergornick@gmail.com>
2023-05-28 13:54:30 -0500

    Added def get_totalPoints():

--- main.py
+++ main.py
@@ -3,9 +3,21 @@
 # Author: Parker Gornick
 # Date: 5/28/23
 
-
 def display_title():
     print("Welcome to the Grade Calculator")
+
+def get_totalPoints():
+    while True:
+        try:
+            total_points = int(input("Enter the total score (0-1000): "))
+            if total_points < 0 or total_points > 1000:
+                print("You must enter an integer value >= 0 and <= 1000. Try Again")
+            else:
+                print(total_points)
+                return total_points
+        except ValueError:
+            print("You must enter an integer value >= 0 and <= 1000. Try Again")
+
 
 
 
@@ -13,3 +25,4 @@ def display_title():
 
 if __name__ == "__main__":
     display_title()
+    get_totalPoints()
